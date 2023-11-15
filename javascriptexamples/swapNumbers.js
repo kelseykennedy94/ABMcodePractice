c12255339cbe18f65cdce1f4fd427e1de6415110
kelseykennedy94 <pken1664@gmail.com>
2023-11-15 15:11:47 -0700

    swap numbers

---
+++ javascriptexamples/swapNumbers.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 10;
+
+console.log(`Before swapping: a = ${a}, b = ${b}`);
+
+// Swapping logic
+let temp = a;
+a = b;
+b = temp;
+
+console.log(`After swapping: a = ${a}, b = ${b}`);
